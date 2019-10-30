70d66844f9e177bdcebe1efb48fb02cb76d9a6ef
Vinz <vincentballut@outlook.fr>
2019-10-30 09:43:51 +0000

    create firsts observers

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,21 @@ import { Observable } from "rxjs";
 
 const observable = Observable.create((observer: any) => {
   observer.next("Hello world");
+  observer.next("Hello again");
+  observer.next("that's work! ");
+  observer.complete();
+  observer.next("that's not work!!!!!!");
 });
 
-observable.subscribe((x: any) => console.log(x));
+observable.subscribe(
+  (x: any) => logItem(x),
+  (error: any) => logItem(`Error: ${error}`),
+  () => logItem("Completed")
+);
+
+function logItem(val: any) {
+  const node = document.createElement("li");
+  const textnode = document.createTextNode(val);
+  node.appendChild(textnode);
+  document.getElementById("list").appendChild(node);
+}
