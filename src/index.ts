2f54ffcb849b334af250095041990681f351c475
Vinz <vincentballut@outlook.fr>
2019-10-30 10:16:34 +0000

    subscribe, unsubscribe

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,47 @@
 import { Observable } from "rxjs";
 
-const observable = Observable.create((observer: any) => {
-  observer.next("Hello world");
-  observer.next("Hello again");
-  observer.next("that's work! ");
-  observer.complete();
-  observer.next("that's not work!!!!!!");
+const observable1 = Observable.create((observer: any) => {
+  observer.next("Observable one is alive");
+  let count = 0;
+  setInterval(() => {
+    observer.next(`Observable ${count}`);
+    ++count;
+  }, 5000);
 });
 
-observable.subscribe(
-  (x: any) => logItem(x),
-  (error: any) => logItem(`Error: ${error}`),
-  () => logItem("Completed")
-);
+const observable2 = Observable.create((observer: any) => {
+  observer.next("Observable two is alive");
+  let count = 0;
+  setInterval(() => {
+    observer.next(`Observable ${count}`);
+    ++count;
+  }, 2500);
+});
 
-function logItem(val: any) {
+const subscription1 = observable1.subscribe((x: any) => logItem(x, 1));
+const subscription2 = observable2.subscribe((x: any) => logItem(x, 2));
+
+document.getElementById("unbtn1").addEventListener("click", () => {
+  subscription1.unsubscribe();
+});
+document.getElementById("unbtn2").addEventListener("click", () => {
+  subscription2.unsubscribe();
+});
+
+document.getElementById("adbtn1").addEventListener("click", () => {
+  subscription1.subscribe();
+});
+document.getElementById("adbtn2").addEventListener("click", () => {
+  subscription2.add(subscription1);
+});
+
+function logItem(val: any, column: any) {
   const node = document.createElement("li");
   const textnode = document.createTextNode(val);
   node.appendChild(textnode);
-  document.getElementById("list").appendChild(node);
+  if (column == 2) {
+    document.getElementById("list2").appendChild(node);
+  } else if (column === 1) {
+    document.getElementById("list1").appendChild(node);
+  }
 }
